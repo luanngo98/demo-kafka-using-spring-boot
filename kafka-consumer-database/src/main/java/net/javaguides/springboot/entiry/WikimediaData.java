6884c6e17abb15264d9aa329f45c96679ca6c396
LuanNT <luanngo98@gmail.com>
2023-01-09 14:54:58 +0700

    first commit

---
+++ kafka-consumer-database/src/main/java/net/javaguides/springboot/entiry/WikimediaData.java
@@ -0,0 +1,18 @@
+package net.javaguides.springboot.entiry;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "wikimedia_recentchange")
+@Data
+public class WikimediaData {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Lob
+    private String wikiEventData;
+}
